fa3c3d5a03bf2871d16dcb3612c243b643bdcb0b
Kevin Wagner <kevin_wagner4493@web.de>
2022-04-08 19:15:49 +0200

    first class implemented

--- js/game.js
+++ js/game.js
@@ -1,13 +1,11 @@
-let canvas;                         // Die Variable für unser "canvas"
-let ctx;                            // ctx = stellt den 2D-Renderkontext für die Zeichenoberfläche eines "canvas" dar.
-let character = new Image()
+let canvas;                                             // Die Variable für unser "canvas"
+let ctx;
+let character = new MovableObject();                    // "character" bekommt die Eigenschaften aus unserer Klasse.
 
 
 function init() {
     canvas = document.getElementById('canvas')
-    ctx = canvas.getContext('2d');
+    ctx = canvas.getContext('2d');                       // getContext('2d') = stellt den 2D-Renderkontext für die Zeichenoberfläche eines "canvas" dar.
 
-    character.src = '../img/2.Secuencias_Personaje-Pepe-correcciขn/2.Secuencia_caminata/W-21.png';
-
-    ctx.drawImage(character, 20, 20, 50, 150);       // "drawImage()" bietet verschiedene Möglichkeiten, ein Bild auf die canvas zu zeichnen.
+    console.log('My Character is', character);
 }
---
+++ models/movable-object.class.js
@@ -0,0 +1,9 @@
+class MovableObject {
+    x = 120;                                // X-Koordinate auf 120px - Standartwert
+    y = 400;                                // Y-Koordinate auf 400px - Standartwert
+    img;
+
+    moveRight() {
+        console.log('Moving right')
+    };
+}
