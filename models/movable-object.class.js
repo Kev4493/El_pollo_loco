3968bd1c479bc169d1e86a1cdc40b6e02e617295
Kevin Wagner <kevin_wagner4493@web.de>
2022-04-15 09:16:22 +0200

    added instance of

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,15 +1,15 @@
 class MovableObject {
-    x = 120;                                // X-Koordinate auf dem Canvas - Standartwert
-    y = 250;                                // Y-Koordinate auf dem Canvas - Standartwert
-    img;                                    // Variable um das aktuelle Bild von Character, Chicken und Cloud zu laden.
+    x = 120;                                                                                      // X-Koordinate auf dem Canvas - Standartwert
+    y = 250;                                                                                      // Y-Koordinate auf dem Canvas - Standartwert
+    img;                                                                                          // Variable um das aktuelle Bild von Character, Chicken und Cloud zu laden.
     height = 150;
     width = 100;
     imageCache = {};
-    currentImage = 0;                       // Dient dazu um die Objekte zu animieren ==> siehe animate()
+    currentImage = 0;                                                                             // Dient dazu um die Objekte zu animieren ==> siehe animate()
     speed = 0.15;
-    otherDirection = false;                 // Variable zum Spiegeln des Bildes. Standartmäßig auf false.
-    speedY = 0;                             // Variable für die Geschwindigkeit des Falls nach unten.
-    acceleration = 2.5;                     // Variable dafür, wie schnell das Objekt beschleunigt wird.
+    otherDirection = false;                                                                       // Variable zum Spiegeln des Bildes. Standartmäßig auf false.
+    speedY = 0;                                                                                   // Variable für die Geschwindigkeit des Falls nach unten.
+    acceleration = 2.5;                                                                           // Variable dafür, wie schnell das Objekt beschleunigt wird.
 
     applyGravity() {
         setInterval(() => {
@@ -17,7 +17,7 @@ class MovableObject {
                 this.y -= this.speedY;
                 this.speedY -= this.acceleration;
             }
-        }, 1000 / 25);                        // wird 25x pro Sekunde ausgeführt.
+        }, 1000 / 25);                                                                            // wird 25x pro Sekunde ausgeführt.
     }
 
     isAboveGround() {
@@ -25,29 +25,31 @@ class MovableObject {
     }
 
 
-    loadImage(path) {                       // Eine Funktion um Bilder zu laden.
-        this.img = new Image();             // Der Variable "img" wird ein neues Bild zugewiesen. // "this." referenziert das Objekt was erstellt wird. // Von Objekt zu Objekt kann das "img" immer einen anderen Wert enthalten. Je nach dem mit welchem Wert das Objekt erstellt wurde.
-        this.img.src = path;                // Wir laden "path" in die Variable (wird dann aus der Klasse Character, Chicken, Clouds... übergeben)
+    loadImage(path) {                                                                             // Eine Funktion um Bilder zu laden.
+        this.img = new Image();                                                                   // Der Variable "img" wird ein neues Bild zugewiesen. // "this." referenziert das Objekt was erstellt wird. // Von Objekt zu Objekt kann das "img" immer einen anderen Wert enthalten. Je nach dem mit welchem Wert das Objekt erstellt wurde.
+        this.img.src = path;                                                                      // Wir laden "path" in die Variable (wird dann aus der Klasse Character, Chicken, Clouds... übergeben)
     }
 
     draw(ctx) {
-        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);                     // Diese Zeile wird für JEDES ELEMENT aus dem jeweiligen Array ausgeführt und zeichnet die IMG's auf das Canvas
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);                         // Diese Zeile wird für JEDES ELEMENT aus dem jeweiligen Array ausgeführt und zeichnet die IMG's auf das Canvas
     }
 
     drawFrame(ctx) {
-        ctx.beginPath();
-        ctx.lineWidth = '5';
-        ctx.strokeStyle = 'blue';
-        ctx.rect(this.x, this.y, this.width, this.height);
-        ctx.stroke();
+        if (this instanceof Character || this instanceof Chicken) {                               // Funktion wird nur für die 2 Klassen angewendet
+            ctx.beginPath();
+            ctx.lineWidth = '5';
+            ctx.strokeStyle = 'blue';
+            ctx.rect(this.x, this.y, this.width, this.height);
+            ctx.stroke();
+        }
     }
 
 
-    loadImages(arr) {                       // Wir bekommen das Array aus loadImages übergeben (bspw. aus Character)
-        arr.forEach((path) => {             // Wir iterrieren durch das Array und bekommen im ersten Durchlauf den ersten Pfad übergeben
-            let img = new Image();          // Wir legen eine Variable an mit einem neuen Bild an                                                               // Variable wird hier innerhalb der Funktion  definiert. Daher kein "this"
-            img.src = path;                 // Wir laden den Pfad in das IMG Objekt
-            this.imageCache[path] = img;    // ImageCache wird geuptdatet und das erste Bild aus dem Array ist nun im ersten Durchlauf im ImageCache           // "this" weil wir auf DIESE Variable oben von dem Objekt zugreifen. Nur mit "this" kann man auf Variablen außerhalb der Funktion zugreifen.
+    loadImages(arr) {                                                                             // Wir bekommen das Array aus loadImages übergeben (bspw. aus Character)
+        arr.forEach((path) => {                                                                   // Wir iterrieren durch das Array und bekommen im ersten Durchlauf den ersten Pfad übergeben
+            let img = new Image();                                                                // Wir legen eine Variable an mit einem neuen Bild an                                                               // Variable wird hier innerhalb der Funktion  definiert. Daher kein "this"
+            img.src = path;                                                                       // Wir laden den Pfad in das IMG Objekt
+            this.imageCache[path] = img;                                                          // ImageCache wird geuptdatet und das erste Bild aus dem Array ist nun im ersten Durchlauf im ImageCache           // "this" weil wir auf DIESE Variable oben von dem Objekt zugreifen. Nur mit "this" kann man auf Variablen außerhalb der Funktion zugreifen.
         });
     }
 
